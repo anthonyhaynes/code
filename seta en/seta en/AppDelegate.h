3abb70e3ee2c8eea1199171e832aee767d23764a
anthonyhaynes <57123548+anthonyhaynes@users.noreply.github.com>
2019-10-29 13:56:36 +0800

    Add files via upload
    首次提交

---
+++ seta en/seta en/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  seta en
+//
+//  Created by iMac3 on 2019/10/29.
+//  Copyright © 2019年 USplanet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ seta en/seta en/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  seta en
+//
+//  Created by iMac3 on 2019/10/29.
+//  Copyright © 2019年 USplanet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
